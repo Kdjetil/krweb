56265999a3dea6780cea8556c4e9988d9829902f
Alex <sasha.sokolov0404@gmail.com>
2023-11-25 11:06:00 +0300

    Поменял на nomenclature

--- src/firebase.ts
+++ src/firebase.ts
@@ -10,5 +10,5 @@ export const firebaseApp = initializeApp({
 // used for the firestore refs
 export const db = getDatabase(firebaseApp,'https://smet-5e7a0-default-rtdb.firebaseio.com/')
 // here we can export reusable database references
-export const studentsTableRef = ref(db, 'nomenclature');
-export const studentById = (index) => {return ref(db,'nomenclature/'+index)}
+export const nomenclaturesTableRef = ref(db, 'nomenclatures');
+export const nomenclatureById = (index) => {return ref(db,'nomenclatures/'+index)}
